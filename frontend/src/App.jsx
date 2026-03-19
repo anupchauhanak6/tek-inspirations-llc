0a79aa7256750348a549fabcd53421c169edb7a9
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-03-19 18:35:40 +0530

    feat: add Header and Footer components to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
 import AppRoutes from './routes/AppRoutes.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
 
   return (
     <>
+      <Header />
       <AppRoutes />
+      <Footer />
     </>
   )
 }
