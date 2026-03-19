a9a3e795f99940c2c5d5d51b9498d34b40f7f338
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-03-20 00:12:03 +0530

    refactor: improve JSX structure in App component for better readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import './App.css'
-import AppRoutes from './routes/AppRoutes.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
+import React from "react";
+import "./App.css";
+import AppRoutes from "./routes/AppRoutes.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-
   return (
-    <>
+    <div className="flex flex-col">
       <Header />
-      <AppRoutes />
+      <main className="pt-20">
+        <AppRoutes />
+      </main>
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
