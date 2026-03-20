eb308d08d57101a5f3da9346c44cc01b4c64cc7c
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-03-21 00:03:40 +0530

    refactor: restructure Home component layout for improved readability and organization

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Hero from "../components/Hero";
+import HomeStats from "../components/HomeStats";
+import ServicesHighlight from "../components/ServicesHighlight";
+import Testimonials from "../components/Testimonials";
+import HomeCTA from "../components/HomeCTA";
 
 function Home() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
+      {/* 1. Hero Section */}
       <Hero />
-      
-    </>
+
+      {/* 2. Stats Banner */}
+      <HomeStats />
+
+      {/* 3. Core Services */}
+      <ServicesHighlight />
+
+      {/* 4. Client Testimonials */}
+      <Testimonials />
+
+      {/* 5. Powerful CTA */}
+      <HomeCTA />
+    </div>
   );
 }
 
